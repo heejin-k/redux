e04852fdf7f5a8e395ab578e4b9d8c6f9a81247e
Sam <wtawonlife@gmail.com>
2023-01-19 02:08:43 +0900

    trimm code

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -1,7 +1,6 @@
-import { useEffect, useState, useContext } from "react";
+import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 function UserInfo() {
   const { authState } = useContext(AuthContext);
